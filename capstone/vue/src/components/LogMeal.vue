f1264979882c4f6d696f3677e9da01710c189341
Daniella Gold <daniellagold29@gmail.com>
2022-09-28 11:59:30 -0400

    started food component

--- capstone/vue/src/components/LogMeal.vue
+++ capstone/vue/src/components/LogMeal.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>Coming Soon</div>
+  <div id="mealpage">
+    <p>Your Food Diary For: {{ new Date()}} </p>
+    <h4>Breakfast</h4>
+      <table v-for="(food, index) in breakfastFoods" v-bind:key="index">
+      {{ food.foodName }}
+      {{ food.caloricAmount }}
+      </table>
+      <button v-on:click="goToAddFood('Breakfast')">Add Food</button>
+    <h4>Lunch</h4>
+      <table v-for="(food, index) in lunchFoods" v-bind:key="index">
+      {{ food.foodName }}
+      {{ food.caloricAmount }}
+      </table>
+      <button >Add Food</button>
+    <h4>Dinner</h4>
+      <table v-for="(food, index) in dinnerFoods" v-bind:key="index">
+      {{ food.foodName }}
+      {{ food.caloricAmount }}
+      </table>
+      <button>Add Food</button>
+    <h4>Snacks</h4>
+      <table v-for="(food, index) in snackFoods" v-bind:key="index">
+      {{ food.foodName }}
+      {{ food.caloricAmount }}
+      </table>
+      <button>Add Food</button>
+    
+    
+    </div>
 </template>
 
 <script>
 export default {
+  name: "log-meal",
+  data(){
+    
+    return {
+      breakfastFoods: [
+        {
+          foodName: 'cereal',
+          caloricAmount: 200
+        },
+        {
+          foodName:'milk',
+          caloricAmount:100
+        }
+      ],
+      lunchFoods: [],
+      dinnerFoods: [],
+      snackFoods: [],
+    }
+  },
+  methods: {
+      goToAddFood(meal){
+
+      }
+  }
 
 }
 </script>
