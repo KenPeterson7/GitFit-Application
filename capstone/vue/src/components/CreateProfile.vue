d80d31b46837314be89884517ae86a9918f4dbc7
Nathan Ballinger <nateballinger@gmail.com>
2022-09-29 21:30:35 -0400

    Implemented add and update goal features. Cleaned up styling.

--- capstone/vue/src/components/CreateProfile.vue
+++ capstone/vue/src/components/CreateProfile.vue
@@ -1,97 +1,192 @@
 <template>
   <div>
-      <form id="newProfileForm" v-on:submit="saveProfile(newProfile)">
-          
-          <div class="form-group">
-              <label for="name">Name: </label>
-          <input id="name" type="text" v-model="newProfile.displayName" />
-          </div>
-          <div class="form-group">
-              <label for="age">Age: </label>
-              <input id="age" type="number" v-model="newProfile.age" />
-          </div>
-          <div class="form-group">
-              <label for="birthday">Birthday: </label>
-              <input id="birthday" type="date" v-model="newProfile.birthday" />
-          </div>
-          <div class="form-group">
-              <label for="height">Height (in inches): </label>
-              <input id="height" type="number" v-model="newProfile.height" />
-          </div>
-          <div class="form-group">
-              <label for="gender">Gender Assigned at Birth: </label>
-              <select id="gender" v-model="newProfile.gender">
-                <option>-</option>
-                <option>M</option>
-                <option>F</option>
-              </select>
-              <p><strong>**This is not used for identification, only for calculating calorie goals**</strong></p>
-          </div>
-          <div class="form-group">
-              <label for="currentWeight">Current Weight (in lbs): </label>
-          <input id="currentWeight" type="number" v-model="newProfile.currentWeight" />
-          </div>
-          <div class="form-group">
-              <label for="desiredWeight">Goal Weight (in lbs): </label>
-          <input id="desiredWeight" type="number" v-model="newProfile.desiredWeight" />
-          </div>
-          <div class="form-group">
-              <label for="activityLevel">Activity Level: </label>
-          <select id="activityLevel" v-model="newProfile.activityLevel">
-              <option>-</option>
-              <option>Sedentary</option>
-              <option>Light</option>
-              <option>Moderate</option>
-              <option>High</option>
-              <option>Extreme</option>
-          </select>
-          </div>
-          <button type="submit">Save Profile</button>
-      </form>
+    <form id="newProfileForm" v-on:submit.prevent="saveProfile()">
+      <div class="form-group">
+        <label for="name">Name: </label>
+        <input id="name" type="text" v-model="newProfile.displayName" />
+      </div>
+      <div class="form-group">
+        <label for="age">Age: </label>
+        <input id="age" type="number" v-model="newProfile.age" />
+      </div>
+      <div class="form-group">
+        <label for="birthday">Birthday: </label>
+        <input id="birthday" type="date" v-model="newProfile.birthday" />
+      </div>
+      <div class="form-group">
+        <label for="height">Height (in inches): </label>
+        <input id="height" type="number" v-model="newProfile.height" />
+      </div>
+      <div class="form-group">
+        <label for="gender">Gender Assigned at Birth: </label>
+        <select id="gender" v-model="newProfile.gender">
+          <option>-</option>
+          <option>M</option>
+          <option>F</option>
+        </select>
+        <p>
+          <strong
+            >**This is not used for identification, only for calculating calorie
+            goals**</strong
+          >
+        </p>
+      </div>
+      <div class="form-group">
+        <label for="currentWeight">Current Weight (in lbs): </label>
+        <input
+          id="currentWeight"
+          type="number"
+          v-model="newProfile.currentWeight"
+        />
+      </div>
+      <div class="form-group">
+        <label for="desiredWeight">Goal Weight (in lbs): </label>
+        <input
+          id="desiredWeight"
+          type="number"
+          v-model="newProfile.desiredWeight"
+        />
+      </div>
+      <div class="form-group">
+        <label for="activityLevel">Activity Level: </label>
+        <select id="activityLevel" v-model="newProfile.activityLevel">
+          <option>-</option>
+          <option>Sedentary</option>
+          <option>Light</option>
+          <option>Moderate</option>
+          <option>High</option>
+          <option>Extreme</option>
+        </select>
+      </div>
+      <div id="form-group">
+        <label for="calorieGoal">Daily Calorie Goal: </label>
+        <input
+          id="calorieGoal"
+          type="number"
+          v-model="newGoal.daily_caloric_goal"
+        />
+        <h6 v-if="requiredDataEntered()" v-on:click="calculateCalorieGoal()">
+          Calculate Suggested Calorie Goal
+        </h6>
+      </div>
+      <button type="submit">Save Profile</button>
+    </form>
   </div>
 </template>
 
 <script>
-import ProfileService from '../services/ProfileService'
+import ProfileService from "../services/ProfileService";
 export default {
-    data() {
-        return {
-            newProfile: {
-                displayName: '',
-                age: '',
-                birthday: '',
-                height: '',
-                gender: '',
-                currentWeight: '',
-                desiredWeight: '',
-                activityLevel: '',
-                starStreak: 0,
-                highStarStreak: 0,
-                profilePic: '',
-                username: this.$store.state.user.username
+  data() {
+    return {
+      newProfile: {
+        displayName: "",
+        age: "",
+        birthday: "",
+        height: "",
+        gender: "-",
+        currentWeight: "",
+        desiredWeight: "",
+        activityLevel: "-",
+        starStreak: 0,
+        highStarStreak: 0,
+        profilePic: "",
+        username: this.$store.state.user.username,
+      },
+      newGoal: {
+        profile_id: "",
+        daily_caloric_goal: "",
+      },
+    };
+  },
+  methods: {
+    saveProfile() {
+        ProfileService.addProfile(this.newProfile).then((response) => {
+            if (response.status == 200) {
+                this.newGoal.profile_id = response.data.profileId;
+                
             }
-        }
+            ProfileService.createGoal(this.newGoal).then((response) => {
+                    if (response.status == 200) {
+                        this.$router.push('/')
+                    }
+                })
+        })
+
     },
-    methods: {
-        saveProfile(profile) {
-            ProfileService.addProfile(profile).then((response) => {
-                if(response==201) {
-                    this.$router.push('/');
-                }
-            })
-        }
-    }
-}
+
+    requiredDataEntered() {
+      return (
+        this.newProfile.currentWeight != "" &&
+        this.newProfile.height != "" &&
+        this.newProfile.age != "" &&
+        this.newProfile.gender != "-" &&
+        this.newProfile.activityLevel != "-"
+      );
+    },
+
+    calculateCalorieGoal() {
+      if (!this.requiredDataEntered()) {
+          this.newGoal.daily_caloric_goal = 0
+         return;
+      }
+      let goal;
+      let BMR;
+      let activityLevel = this.newProfile.activityLevel;
+      if (this.newProfile.gender == "M") {
+        BMR =
+          66.5 +
+          6.1875 * this.newProfile.currentWeight +
+          12.7 * this.newProfile.height -
+          6.75 * this.newProfile.age;
+      }
+      if (this.newProfile.gender == "F") {
+        BMR =
+          655.1 +
+          4.3 * this.newProfile.currentWeight +
+          4.7 * this.newProfile.height -
+          4.676 * this.newProfile.age;
+      }
+      if (activityLevel == "Sedentary") {
+        goal = BMR * 1.2;
+      }
+      if (activityLevel == "Light") {
+        goal = BMR * 1.375;
+      }
+      if (activityLevel == "Moderate") {
+        goal = BMR * 1.55;
+      }
+      if (activityLevel == "High") {
+        goal = BMR * 1.725;
+      }
+      if (activityLevel == "Extreme") {
+        goal = BMR * 1.9;
+      }
+      this.newGoal.daily_caloric_goal = Math.round(goal);
+
+    //   return Math.round(goal);
+    },
+  },
+};
 </script>
 
-<style>
-#newProfileForm{
-    display: flex;
-    flex-direction: column;
-    margin:20px;
+<style scoped>
+#newProfileForm {
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
 }
 
 label {
-    margin-right: 5px;
+  margin-right: 5px;
+}
+
+h6 {
+  text-align: center;
+  border: 3px;
+  border-style: solid;
+  padding: 3px;
+  background-color: rgb(0, 125, 255);
+  color: lightgray;
 }
 </style>
--- capstone/vue/src/services/ProfileService.js
+++ capstone/vue/src/services/ProfileService.js
@@ -12,5 +12,20 @@ export default {
 
     addProfile(profile) {
         return axios.post('/profile', profile)
+    },
+    getGoal(profileId){
+        return axios.get(`/profile/goal/${profileId}`)
+    },
+
+    createGoal(goal){
+        return axios.post('/profile/goal', goal)
+    },
+
+    updateGoal(profileId,goal){
+        return axios.put(`/profile/goal/${profileId}`, goal)
+    },
+
+    deleteGoal(profileId){
+        return axios.delete(`/profile/goal/${profileId}`)
     }
 }
